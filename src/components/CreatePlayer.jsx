7a730022c5093344e016625254bd7247c584a06a
Jessica Spivak <jls103193@yahoo.com>
2023-08-02 08:50:28 -0400

    working on create player form, issue with BrowserRouter import

---
+++ src/components/CreatePlayer.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+const APIPlayersURL = "https://fsa-puppy-bowl.herokuapp.com/api/2302-acc-et-web-pt-a/players";
+
+export default function CreatePlayer() {
+    const [name, setName] = useState("");
+    const [breed, setBreed] = useState("");
+    const [status, setStatus] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [teamId, setTeamId] = useState(null);
+    // const [error, setError] = useState(null);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch (
+                APIPlayersURL,
+                {
+                    method: "POST"
+                    body: JSON.stringify({ name, breed, status, imageUrl, teamId }),
+                }
+            );
+            const result = await response.json();
+        } catch (error) {
+            console.error('Oops, something went wrong with adding that player!', error);
+        }
+        setName("");
+        setBreed("");
+        setStatus("");
+        setImageUrl("");
+        setTeamId(null);
+    }
+    return (
+        <div>
+            <h2>Create Puppy Player</h2>
+        </div>
+    )
+}
