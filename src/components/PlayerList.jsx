82d5ded65dcb5eb104078f6160cb99a3662e4b40
Jessica Spivak <jls103193@yahoo.com>
2023-08-03 13:05:50 -0400

    search bar somewhat complete. still need see details and delete buttons functioning and a return to full list type button to get out of the searched for  puppy

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -3,13 +3,15 @@ import { useState, useEffect } from 'react';
 const APIPlayersURL = "https://fsa-puppy-bowl.herokuapp.com/api/2302-acc-et-web-pt-a/players";
 
 export default function PlayerList() {
+    const [allPlayers, setAllPlayers] = useState([]);
     const [players, setPlayers] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
     useEffect(() => {
         async function fetchAllPlayers() {
             try {
                 const response = await fetch(APIPlayersURL);
                 const players = await response.json();
-                console.log(players);
+                setAllPlayers(players.data.players)
                 setPlayers(players.data.players)
             } catch (error) {
                 console.error('Uh oh, trouble fetching players!', error);
@@ -17,6 +19,25 @@ export default function PlayerList() {
         }
         fetchAllPlayers();
     }, [])
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const filteredPlayers = allPlayers.filter(player =>
+                player.name.toLowerCase().includes(searchQuery.toLowerCase())
+            )
+            setPlayers(filteredPlayers)
+        } catch (error) {
+            console.error('Uh oh, trouble searching for players!', error);
+        }
+    };
+
+    const handleChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+
+
     const showPlayers = () => {
         if (players) {
             return <>
@@ -24,8 +45,6 @@ export default function PlayerList() {
                     <div key={player.id}>
                         <div><b>{player.name}</b></div>
                         <div><img src={player.imageUrl} /></div>
-                        <button className="seeDetails">See Details</button>
-                        <button className="deletePuppy">Delete Puppy</button>
                         <br />
                     </div>
                 )}
@@ -34,10 +53,43 @@ export default function PlayerList() {
         return <div />
     }
 
+    const showSearchBar = () => {
+        return <form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center' }}>
+            <input
+                type="text"
+                value={searchQuery}
+                onChange={handleChange}
+                placeholder="Enter your player name..."
+                style={{
+                    padding: '8px',
+                    fontSize: '16px',
+                    borderRadius: '4px',
+                    border: '1px solid #ccc',
+                }}
+            />
+            <button
+                type="submit"
+                style={{
+                    marginLeft: '8px',
+                    padding: '8px 12px',
+                    fontSize: '16px',
+                    borderRadius: '4px',
+                    background: 'blue',
+                    color: '#fff',
+                    border: 'none',
+                    cursor: 'pointer',
+                }}
+            >
+                Search
+            </button>
+        </form>
+    }
+
 
     return (
         <div className="playerList">
             <h3>Players List</h3>
+            {showSearchBar()}
             {showPlayers()}
         </div>
     )
